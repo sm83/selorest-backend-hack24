9641ee17c0d4e552b1f9e5bd4ed0d9d7483eae3b
p-shcherbatykh <p.shcherbatykh@redlight.group>
2024-11-22 16:49:07 +0300

    minor fixes

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from './roles.model';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
-  imports: [SequelizeModule.forFeature([Role])],
+  imports: [SequelizeModule.forFeature([Role]), forwardRef(() => AuthModule)],
   exports: [RolesService],
 })
 export class RolesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,16 @@ import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User]), forwardRef(() => RolesModule)],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    forwardRef(() => RolesModule),
+    forwardRef(() => AuthModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
